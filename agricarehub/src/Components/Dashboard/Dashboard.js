c921bfdf80df44c0ff95673da77daafa4b82cc44
MD. Shojib Talukder <109905033+AdilHasanShojib@users.noreply.github.com>
2024-03-24 22:55:12 +0600

    Dashboard In Progress

---
+++ agricarehub/src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./dashboard.css";
+
+const Dashboard = () => {
+  // const [successMessage, setSuccessMessage] = useState('');
+  // setSuccessMessage('Signup successful!');
+  return (
+    <>
+      
+    </>
+  );
+};
+
+export default Dashboard;
