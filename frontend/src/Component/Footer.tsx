723df1ae6e6d6c1f440cdb0f9323486ace583e65
samratabduljalil <samratabduljalil21@gmail.com>
2024-04-04 06:13:02 +0600

    working on navbar and footer

---
+++ frontend/src/Component/Footer.tsx
@@ -0,0 +1,8 @@
+function Footer(){
+
+    return <><h1>I am footer</h1></>
+    
+    
+    
+    }
+    export default Footer
---
+++ frontend/src/Component/Navbar.tsx
@@ -0,0 +1,8 @@
+function Navbar (){
+
+return <><h1>I am navbar</h1></>
+
+
+
+}
+export default Navbar
