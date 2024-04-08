6f8e96311f21e33e16c6e0cae5a64847ffcccb61
samratabduljalil <samratabduljalil21@gmail.com>
2024-04-08 11:22:11 +0600

    tailwind added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
-import './App.css'
+
 import Navbar from './Component/Navbar'
 import Footer from './Component/Footer'
+import Modal from './Component/Modal'
 import Home from './Pages/Home'
 
 function App() {
   
   return (
     <>
+  
      <Navbar></Navbar>
      <Home/>
      <Footer></Footer>
+     <Modal></Modal>
+     <h1 className='text-white'>hello</h1>
      
     </>
   )
--- frontend/src/Component/Modal.tsx
+++ frontend/src/Component/Modal.tsx
@@ -1,18 +1,20 @@
-// Modal.js
-import React from 'react';
-import './modal.css'; // Import CSS for styling the modal
 
-const Modal = ({ isOpen, onClose, children }) => {
-  if (!isOpen) return null;
 
-  return (
-    <div className="modal-overlay" onClick={onClose}>
-      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-        <span className="close-button" onClick={onClose}>X</span>
-        {children}
-      </div>
-    </div>
-  );
-};
 
-export default Modal;
+function Modal() {
+
+    return (
+        <>
+          
+
+           <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm">
+           
+           
+
+            </div>
+        </>
+
+    )
+
+}
+export default Modal
